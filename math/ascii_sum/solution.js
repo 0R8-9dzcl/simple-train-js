1af5831a1d80d3014b43cbaaf8222789fb5af1b9
Alexeev Eugene <alekseev.evgeniy.wd@yandex.ru>
2023-10-25 02:56:51 +0300

    add: Задача: Рассчитайте сумму ASCII представления символов в строке.

---
+++ math/ascii_sum/solution.js
@@ -0,0 +1,18 @@
+/**
+ * Задача: Рассчитайте сумму ASCII представления символов в строке.
+ *
+ * @param {string} word - Входная строка.
+ * @returns {number} - Сумма ASCII представления символов в строке.
+ *
+ * Примеры:
+ * solution("test");      // Должно вернуть 448
+ * solution("clojure");   // Должно вернуть 756
+ * solution("frontend");  // Должно вернуть 864
+ */
+
+const solution = (word = '') => {
+  let result = 0;
+  return result;
+};
+
+module.exports = solution;
---
+++ math/ascii_sum/test.js
@@ -0,0 +1,37 @@
+const runTests = require('../../testFunction');
+const solution = require('./solution'); // Импортируем функцию из solution.js
+
+const tests = [
+  { expected: 864, args: ['frontend'] },
+  { expected: 756, args: ['clojure'] },
+  { expected: 448, args: ['test'] },
+  { expected: 1081, args: ['attraction'] },
+  { expected: 444, args: ['jump'] },
+  { expected: 429, args: ['milk'] },
+  { expected: 411, args: ['care'] },
+  { expected: 539, args: ['brass'] },
+  { expected: 440, args: ['iron'] },
+  { expected: 332, args: ['ray'] },
+  { expected: 444, args: ['jump'] },
+  { expected: 428, args: ['rate'] },
+  { expected: 536, args: ['drink'] },
+  { expected: 875, args: ['mountain'] },
+  { expected: 424, args: ['food'] },
+  { expected: 875, args: ['mountain'] },
+  { expected: 518, args: ['death'] },
+  { expected: 853, args: ['reaction'] },
+  { expected: 549, args: ['crush'] },
+  { expected: 437, args: ['look'] },
+  { expected: 541, args: ['paste'] },
+  { expected: 455, args: ['snow'] },
+  { expected: 422, args: ['sand'] },
+  { expected: 541, args: ['paste'] },
+  { expected: 428, args: ['rate'] },
+  { expected: 975, args: ['amusement'] },
+  { expected: 878, args: ['interest'] },
+  { expected: 439, args: ['burn'] },
+  { expected: 539, args: ['brass'] },
+  { expected: 875, args: ['mountain'] },
+];
+
+runTests(tests, solution);
